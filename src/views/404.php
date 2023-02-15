ff05f736ea0c6c51af0b421297abbc72683c286d
Vincent Sureau <vincentsureau5@gmail.com>
2023-02-15 14:11:40 +0100

    ajouté router de base

---
+++ src/views/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Accueil !</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    </head>
+    <body>
+        <?php include "_navbar.php" ?>
+        <section class="section">
+            <div class="container">
+                <div class="notification is-danger">
+                Erreur 404 ! <strong>La page demandée n'existe pas</strong>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
