48ed72ae926687acaf7b01660099521e443e9085
Vincent Sureau <vincentsureau5@gmail.com>
2023-02-16 09:37:57 +0100

    separated sql request in model

--- src/controllers/ProjectsController.php
+++ src/controllers/ProjectsController.php
@@ -1,23 +1,17 @@
 <?php
 
-require_once __DIR__ . '/../utils/database.php';
+require_once __DIR__ . '/../models/Projet.php';
 
 dump($_POST);
 
 $title = $_POST['title'] ?? null;
 $description = $_POST['description'] ?? null;
 
-$pdo = getConnection();
 if(!empty($title)) {
-    $sql = "INSERT INTO `Project` (`id`, `description`, `title`, `user_id`) VALUES (NULL,'$description', '$title', NULL);";
-    $pdoStatement = $pdo->prepare($sql);
-    $result = $pdoStatement->execute();
+    create($title, $description);
 }
 
-$sql = "SELECT * FROM `Project`;";
-$pdoStatement = $pdo->prepare($sql);
-$result = $pdoStatement->execute();
-$projects = $pdoStatement->fetchAll();
-$projects = array_chunk($projects, 3);
+
+$projects = array_chunk(findAll(), 3);
 
 include __DIR__ .'/../views/home.php';
